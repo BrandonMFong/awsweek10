30aca3419caf0c3433867ce889e00869be0d6195
BrandonMFong <bfong@owc.com>
2021-11-07 22:58:37 +0000

    Adding endpoint for ec2

--- endpoints.tf
+++ endpoints.tf
@@ -37,3 +37,16 @@ resource "aws_security_group" "week10-https-sg" {
   }
 }
 
+# EC2
+resource "aws_vpc_endpoint" "week10-ec2-ep" {
+  vpc_id            = aws_vpc.week10-vpc.id
+  service_name      = "com.amazonaws.us-east-1.ec2"
+  vpc_endpoint_type = "Interface"
+  subnet_ids = [aws_subnet.week10-pri-a.id, aws_subnet.week10-pri-b.id]
+
+  security_group_ids = [
+    aws_security_group.week10-https-sg.id,
+  ]
+
+  private_dns_enabled = true
+}
