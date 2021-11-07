bdcefe0a349a5561f7ad746331e22abce82972c4
BrandonMFong <bfong@owc.com>
2021-11-07 23:00:48 +0000

    Adding endpoints for sqs

--- endpoints.tf
+++ endpoints.tf
@@ -50,3 +50,18 @@ resource "aws_vpc_endpoint" "week10-ec2-ep" {
 
   private_dns_enabled = true
 }
+
+# SQS
+resource "aws_vpc_endpoint" "week10-sqs-ep" {
+  vpc_id            = aws_vpc.week10-vpc.id
+  service_name      = "com.amazonaws.us-east-1.sqs"
+  vpc_endpoint_type = "Interface"
+  subnet_ids = [aws_subnet.week10-pri-a.id, aws_subnet.week10-pri-b.id]
+
+  security_group_ids = [
+    aws_security_group.week10-https-sg.id,
+  ]
+
+  private_dns_enabled = true
+}
+
